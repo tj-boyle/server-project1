f7109c2bbbd9985198af9dfd8b6012f42078b18e
Thomas Boyle <tjb2597@rit.edu>
2014-03-03 01:04:50 -0500

    Basic layout

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,9 +1,14 @@
 <header>
-    <nav>
-        <ul>
-            <li><a href='#'></a></li>
-            <li><a href='#'></a></li>
-            <li><a href='#'></a></li>
-        </ul>
-    </nav>
+    <div class='container'>
+        <div class='sixteen columns'>
+            <h2 class='left'>Stuffed Animals</h2>
+            <nav class='right'>
+                <ul>
+                    <li><a <?php if($current == "home") { echo("class='current'"); } ?> href='#'>HOME</a></li>
+                    <li><a <?php if($current == "cart") { echo("class='current'"); } ?> href='#'>CART</a></li>
+                    <li><a <?php if($current == "admin"){ echo("class='current'"); } ?> href='#'>ADMIN</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </header>
--- index.php
+++ index.php
@@ -4,7 +4,49 @@
 <?php include_once('assets/includes/head.php'); ?>
 
 <body>
-    <?php include_once('assets/includes/header.php'); ?>
+    <?php
+        $current = "home"; 
+        include_once('assets/includes/header.php'); 
+    ?>
 
+    <div class='container animals'>
+        <div class='eight columns animal'>
+            <div class='four columns alpha'>
+                <div class='a-image'></div>
+            </div>
+            <div class='four columns omega info'>
+                <h4>Bear</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum velit eu sollicitudin ornare. Integer sit amet odio quis tortor tincidunt suscipit et eu ligula.</p>
+                <input type='button' value='Add To Cart'>
+            </div>
+        </div>
+        <div class='eight columns animal'>
+            <div class='a-image'></div>
+        </div>
+        <div class='eight columns animal'>
+            <div class='a-image'></div>
+        </div>
+        <div class='eight columns animal'>
+            <div class='a-image'></div>
+        </div>
+        <div class='eight columns animal'>
+            <div class='a-image'></div>
+        </div>
+        <div class='eight columns animal'>
+            <div class='a-image'></div>
+        </div>
+    </div>
+    
+    <?php
+
+        // include_once('credentials.php');
+        // $con=mysql_connect("127.0.0.1",$username,$password)
+        //     or die("couldn't connect: ".mysql_error());
+        // mysql_select_db("tjb2597");
+
+        // $Query = "select * from stuffed-animal";
+        // //Goes through query results
+        // $v_TheResult = mysql_query ($Query); 
+    ?>
 </body>
 </html>
