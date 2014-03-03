50c14483e08d85b2dc0efaa2dad2281f9dd5ad90
Thomas Boyle <tjb2597@rit.edu>
2014-03-02 22:38:43 -0500

    First commit

---
+++ assets/includes/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Project 1</title>
+    <link rel="stylesheet" type="text/css" href="./assets/stylesheets/base.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/stylesheets/skeleton.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/stylesheets/layout.css"/>
+    <!--[if IE]>
+      <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,800' rel='stylesheet' type='text/css'> -->
+</head>
---
+++ assets/includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href='#'></a></li>
+            <li><a href='#'></a></li>
+            <li><a href='#'></a></li>
+        </ul>
+    </nav>
+</header>
